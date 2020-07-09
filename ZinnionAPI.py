2cc6dd8ec64584d5c951255651cdf4678c71c7ac
Mauro Delazeri <maurodelazeri@gmail.com>
2020-07-09 13:19:44 -0400

    update

--- ZinnionAPI.py
+++ ZinnionAPI.py
@@ -1,16 +1,17 @@
 import sys
 import logging
-import zmq
 import ctypes
 import os
 import signal
 import pandas
 import time
+import json
 
 
 class ZinnionAPI(object):
     def __init__(self):
         format = "%(asctime)s: %(message)s"
+        self.mauro = 0
         logging.basicConfig(
             format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
@@ -24,14 +25,15 @@ class ZinnionAPI(object):
         elif sys.platform == "darwin":
             # OS X
             logging.info("Python ZTrading    : Plataform: OS X")
-            sys.exit()
+            # sys.exit()
         elif sys.platform == "win32":
             # Windows...
             logging.info("Python ZTrading    : sys.platform not supported")
             sys.exit()
 
-        if 'ALGORITHM_ID' not in os.environ:
-            logging.info("Python ZTrading    : ALGORITHM_ID: not set")
+        if 'DEPLOYMENT_ID' not in os.environ:
+            logging.info(
+                "Python ZTrading    : DEPLOYMENT_ID : not set")
             sys.exit()
 
         if 'TOKEN' not in os.environ:
@@ -66,84 +68,84 @@ class ZinnionAPI(object):
         if self.ztrading_lib.add_streaming(bytes(streaming_config, 'utf-8')) == False:
             os.kill(os.getpid(), signal.SIGUSR1)
 
-    def add_indicator(self, indicator_config):
-        self.ztrading_lib.add_indicator.restype = ctypes.c_bool
-        if self.ztrading_lib.add_indicator(bytes(indicator_config, 'utf-8')) == False:
+    def add_indicators(self, indicators_config):
+        self.ztrading_lib.add_indicators.restype = ctypes.c_bool
+        if self.ztrading_lib.add_indicators(bytes(indicators_config, 'utf-8')) == False:
             os.kill(os.getpid(), signal.SIGUSR1)
 
-    def get_history_candles(self, bart_ype, venue, product, step, num_candles, begin, end):
-        self.ztrading_lib.get_history_candles.restype = ctypes.c_char_p
-        data = self.ztrading_lib.get_history_candles(
-            bytes(bart_ype, 'utf-8'), bytes(venue, 'utf-8'), bytes(product, 'utf-8'), step, num_candles, bytes(begin, 'utf-8'), bytes(end, 'utf-8'))
-        pd = pandas.read_json(data)
-        return pd
+    def send_order(self, order):
+        self.ztrading_lib.order_send.restype = ctypes.c_char_p
+        result = self.ztrading_lib.order_send(bytes(order, 'utf-8'))
+        json_object = json.loads(result)
+        return json.dumps(json_object, indent=2)
 
-    def get_history_trades(self, venue, product, begin, end):
-        self.ztrading_lib.get_history_trades.restype = ctypes.c_char_p
-        data = self.ztrading_lib.get_history_trades(
-            bytes(venue, 'utf-8'), bytes(product, 'utf-8'), bytes(begin, 'utf-8'), bytes(end, 'utf-8'))
-        pd = pandas.read_json(data)
-        return pd
+    def close_position(self, position):
+        self.ztrading_lib.close_position.restype = ctypes.c_char_p
+        result = self.ztrading_lib.close_position(bytes(position, 'utf-8'))
+        json_object = json.loads(result)
+        return json.dumps(json_object, indent=2)
 
-    def get_indicator(self, input_config):
-        self.ztrading_lib.get_indicator.restype = ctypes.c_char_p
-        data = self.ztrading_lib.get_indicator(
-            bytes(input_config, 'utf-8'), self.debug)
-        pd = pandas.read_json(data)
-        if pd.empty:
-            return pandas.DataFrame()
-        return pd
+    def modify_position(self, position):
+        self.ztrading_lib.modify_position.restype = ctypes.c_char_p
+        result = self.ztrading_lib.modify_position(bytes(position, 'utf-8'))
+        json_object = json.loads(result)
+        return json.dumps(json_object, indent=2)
 
-    def get_candlestick_buffer(self, venue, product, candle_type, timeframe):
-        self.ztrading_lib.get_candlestick_buffer.restype = ctypes.c_char_p
-        data = self.ztrading_lib.get_candlestick_buffer(
-            bytes(venue, 'utf-8'), bytes(product, 'utf-8'), bytes(candle_type, 'utf-8'), timeframe)
-        pd = pandas.read_json(data)
-        if pd.empty:
-            return pandas.DataFrame()
-        return pd
+    def cancel_order(self, order):
+        # json_object = json.loads(order)
+        # return json.dumps(json_object, indent=2)
+        self.ztrading_lib.cancel_order.restype = ctypes.c_char_p
+        result = self.ztrading_lib.cancel_order(bytes(order, 'utf-8'))
+        json_object = json.loads(result)
+        return json.dumps(json_object, indent=2)
 
-    def get_counter(self):
-        self.ztrading_lib.get_counter.restype = ctypes.c_int
-        data = self.ztrading_lib.get_counter()
-        print(data)
+    def get_orders(self, req):
+        result = self.ztrading_lib.get_orders(bytes(req, 'utf-8'))
+        json_object = json.loads(ctypes.c_char_p(result).value.decode("utf-8"))
+        return json.dumps(json_object, indent=2)
+
+    def get_positions(self, req):
+        result = self.ztrading_lib.get_positions(bytes(req, 'utf-8'))
+        json_object = json.loads(ctypes.c_char_p(result).value.decode("utf-8"))
+        return json.dumps(json_object, indent=2)
+
+    def get_deals(self, req):
+        result = self.ztrading_lib.get_deals(bytes(req, 'utf-8'))
+        json_object = json.loads(ctypes.c_char_p(result).value.decode("utf-8"))
+        return json.dumps(json_object, indent=2)
 
     def hand_data(self, callback, json):
         callback(self, json)
 
     def start_streaming(self, callback):
-        logging.info("Python ZTrading    : Stream startup")
-        # Prepare our context and publisher
-        context = zmq.Context()
-        subscriber = context.socket(zmq.SUB)
-
-        if 'ZTRADING_STREAM_ADDR' in os.environ:
-            subscriber.connect(os.environ['ZTRADING_STREAM_ADDR'])
-        else:
-            subscriber.connect("tcp://localhost:5555")
-
-        subscriber.setsockopt(zmq.SUBSCRIBE, b"")
-
         if self.simulation == True:
             logging.info("Python ZTrading    : SIMULATION MODE")
-            self.ztrading_lib.nex_simulation_msg.restype = ctypes.c_bool
-            if self.ztrading_lib.nex_simulation_msg() == False:
-                os.kill(os.getpid(), signal.SIGTERM)
-
         while True:
-            # Read envelope with address
-            msg = subscriber.recv_json()
-            # Request next message if we are in simulation mode
-            if self.simulation == True:
-                if 'type' in msg:
-                    if msg['type'] == 1 or msg['type'] == 7:
-                        self.ztrading_lib.nex_simulation_msg.restype = ctypes.c_bool
-                        if self.ztrading_lib.nex_simulation_msg() == False:
-                            os.kill(os.getpid(), signal.SIGTERM)
-
-            # Handle the data received
-            self.hand_data(callback, msg)
-
-        # We never get here but clean up anyhow
-        subscriber.close()
-        context.term()
+            try:
+                self.ztrading_lib.get_next_msg.restype = ctypes.c_char_p
+                result = self.ztrading_lib.get_next_msg()
+                json_object = json.loads(result)
+                # json_formatted_str = json.dumps(json_object, indent=2)
+                # Handle the data received
+                if self.simulation == True:
+                    if "comment" in json_object:
+                        if json_object["comment"] == "Finished":
+                            libdl = ctypes.CDLL(os.environ['ZTRADING_LIB'])
+                            libdl.dlclose(self.ztrading_lib._handle)
+                            break
+                self.hand_data(callback, json_object)
+            except KeyboardInterrupt:
+                libdl = ctypes.CDLL(os.environ['ZTRADING_LIB'])
+                libdl.dlclose(self.ztrading_lib._handle)
+                e = sys.exc_info()[0]
+                print('KeyboardInterrupt!')
+                print("An exception occurred", e, " data: ", result)
+                os._exit(0)
+            except:
+                libdl = ctypes.CDLL(os.environ['ZTRADING_LIB'])
+                libdl.dlclose(self.ztrading_lib._handle)
+                e = sys.exc_info()[0]
+                print("An exception occurred", e, " data: ", result)
+                os._exit(0)
+        logging.info("Python ZTrading    : FINISHED")
+        os._exit(0)
